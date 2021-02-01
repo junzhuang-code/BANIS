9242f62f64684ed3447f86c95ab2f81c4f3621e9
Jun Zhuang <65136293+junzhuang-code@users.noreply.github.com>
2021-02-01 14:42:59 -0500

    Update BANIS model file

--- banis/banis_model.py
+++ banis/banis_model.py
@@ -109,7 +109,7 @@ class BANIS():
         model.add(BatchNormalization(momentum=0.8))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Reshape((dim, dim, depth)))
-        assert model.output_shape == (None, dim, dim, depth) # 注意：batch size 没有限制
+        assert model.output_shape == (None, dim, dim, depth)
         model.add(Conv2DTranspose(depth, (5, 5), strides=(1, 1), padding='same', use_bias=False, kernel_initializer='he_normal'))
         assert model.output_shape == (None, dim, dim, depth)
         model.add(BatchNormalization(momentum=0.8))
@@ -136,7 +136,7 @@ class BANIS():
         model.add(Conv2D(2*depth, (5, 5), strides=(2, 2), padding='same', kernel_initializer='he_normal'))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(drop_rate))
-        #model.add(Conv2D(4*depth, (5, 5), strides=(2, 2), padding='same', kernel_initializer='he_normal')) # 新增 ---
+        #model.add(Conv2D(4*depth, (5, 5), strides=(2, 2), padding='same', kernel_initializer='he_normal'))
         #model.add(LeakyReLU(alpha=0.2))
         #model.add(Dropout(drop_rate)) # ---
         # Out: 1-dim probability
@@ -145,6 +145,7 @@ class BANIS():
         model.add(Activation('sigmoid'))
         model.summary()
         return model
+
 
     def train(self, A, B, EPOCHS=100, BATCH_SIZE=128, WARMUP_STEP=20, NUM_IMG=5):
         # Define the groundtruth
@@ -156,7 +157,7 @@ class BANIS():
         summary_writer = tf.summary.create_file_writer(self.log_dir)
 
         # Initialize the checkpoint
-        interval = int(EPOCHS // 10) if EPOCHS >= 10 else 5
+        interval = int(EPOCHS // 5) if EPOCHS >= 10 else 5
         checkpoint_path = os.path.join(self.checkpoint_dir, "ckpt")
         checkpoint = tf.train.Checkpoint(E_optimizerA=self.E_optA,
                                          G_optimizerA=self.G_optA,
@@ -182,112 +183,117 @@ class BANIS():
 
         A_gen_list, B_gen_list, AB_rec_list = [], [], []
         match_cnt, total_cnt = 0, 0 # Initialize the counting for matching DSC
+        NUM_BATCH = len(A) // BATCH_SIZE  # np.ceil()
         for epoch in range(EPOCHS):
-            # ---Pretrain Stage ---
-            # Select random batch of real instances
-            idx = np.random.randint(0, A.shape[0], size=BATCH_SIZE)
-            A_real = A[idx, :, :, :]
-            # Generate a batch of latent variables based on uniform distribution
-            z_A = np.random.uniform(-1.0, 1.0, size=[BATCH_SIZE, self.latent_dim])
-            A_gen = self.generatorA.predict(z_A)
-            # Train the discriminator (real for 1 and rec for 0) ------
-            A_both = np.concatenate((A_real, A_gen))
-            Dloss_A = self.discriminatorA.train_on_batch(A_both, Y_both)
-            # Train the pioneer model to fool the discriminator ------
-            Gloss_A = self.pioneerA.train_on_batch(z_A, Y_real)
+            for nb in range(NUM_BATCH-1):
+                # ---Pretrain Stage ---
+                # Select real instances batch by batch
+                step = int(epoch * NUM_BATCH + nb)
+                idx = np.arange(nb*BATCH_SIZE, nb*BATCH_SIZE+BATCH_SIZE)
+                A_real = A[idx, :, :, :]
 
-            # Repeat the same procedure as above for B
-            B_real = B[idx, :, :, :]
-            z_B = np.random.uniform(-1.0, 1.0, size=[BATCH_SIZE, self.latent_dim])
-            B_gen = self.generatorB.predict(z_B)  
-            B_both = np.concatenate((B_real, B_gen))
-            Dloss_B = self.discriminatorB.train_on_batch(B_both, Y_both)
-            Gloss_B = self.pioneerB.train_on_batch(z_B, Y_real)
+                # Generate a batch of latent variables based on uniform distribution
+                z_A = np.random.uniform(-1.0, 1.0, size=[BATCH_SIZE, self.latent_dim])
+                A_gen = self.generatorA.predict(z_A)
+                # Train the discriminator (real for 1 and rec for 0) ------
+                A_both = np.concatenate((A_real, A_gen))
+                Dloss_A = self.discriminatorA.train_on_batch(A_both, Y_both)
+                # Train the pioneer model to fool the discriminator ------
+                Gloss_A = self.pioneerA.train_on_batch(z_A, Y_real)
 
-            # Train the successor & coordinator when epoch > WARMUP_STEP ------
-            mse = -1
-            if epoch > WARMUP_STEP:
-                mseA_gen = self.successorA.train_on_batch(B_real, A_gen)
-                mseB_gen = self.successorB.train_on_batch(A_real, B_gen)
-                mseA_real = self.successorA.train_on_batch(B_real, A_real)
-                mseB_real = self.successorB.train_on_batch(A_real, B_real)
-                mse_B2B = self.coordinatorA.train_on_batch(B_real, B_real)
-                mse_A2A = self.coordinatorB.train_on_batch(A_real, A_real)
-                mse_A = 0.5 * np.add(mseA_real, mseA_gen)
-                mse_B = 0.5 * np.add(mseB_real, mseB_gen)
-                identity_loss = 0.5 * np.add(mse_A, mse_B)
-                pair_matched_loss = 0.5 * np.add(mse_A2A, mse_B2B)
-                mse = np.mean([identity_loss, pair_matched_loss], axis=0)
+                # Repeat the same procedure as above for B
+                B_real = B[idx, :, :, :]
+                z_B = np.random.uniform(-1.0, 1.0, size=[BATCH_SIZE, self.latent_dim])
+                B_gen = self.generatorB.predict(z_B)  
+                B_both = np.concatenate((B_real, B_gen))
+                Dloss_B = self.discriminatorB.train_on_batch(B_both, Y_both)
+                Gloss_B = self.pioneerB.train_on_batch(z_B, Y_real)
 
-            # For Experiments and Visualization ---------------------
-            # Save scalars into TensorBoard
-            with summary_writer.as_default():
-                tf.summary.scalar('D_loss_A', Dloss_A[0], step=epoch)
-                tf.summary.scalar('G_loss_A', Gloss_A[0], step=epoch)
-                tf.summary.scalar('D_acc_A', Dloss_A[1], step=epoch)
-                tf.summary.scalar('D_loss_B', Dloss_B[0], step=epoch)
-                tf.summary.scalar('G_loss_B', Gloss_B[0], step=epoch)
-                tf.summary.scalar('D_acc_B', Dloss_B[1], step=epoch)
+                # Train the successor & coordinator when epoch > WARMUP_STEP ------
+                mse = -1
+                if epoch > WARMUP_STEP:
+                    mseA_gen = self.successorA.train_on_batch(B_real, A_gen)
+                    mseB_gen = self.successorB.train_on_batch(A_real, B_gen)
+                    mseA_real = self.successorA.train_on_batch(B_real, A_real)
+                    mseB_real = self.successorB.train_on_batch(A_real, B_real)
+                    mse_B2B = self.coordinatorA.train_on_batch(B_real, B_real)
+                    mse_A2A = self.coordinatorB.train_on_batch(A_real, A_real)
+                    mse_A = 0.5 * np.add(mseA_real, mseA_gen)
+                    mse_B = 0.5 * np.add(mseB_real, mseB_gen)
+                    identity_loss = 0.5 * np.add(mse_A, mse_B)
+                    pair_matched_loss = 0.5 * np.add(mse_A2A, mse_B2B)
+                    mse = np.mean([identity_loss, pair_matched_loss], axis=0)
+
+                # For Experiments and Visualization ---------------------
+                # Save scalars into TensorBoard
+                with summary_writer.as_default():
+                    tf.summary.scalar('D_loss_A', Dloss_A[0], step=step)
+                    tf.summary.scalar('G_loss_A', Gloss_A[0], step=step)
+                    tf.summary.scalar('D_acc_A', Dloss_A[1], step=step)
+                    tf.summary.scalar('D_loss_B', Dloss_B[0], step=step)
+                    tf.summary.scalar('G_loss_B', Gloss_B[0], step=step)
+                    tf.summary.scalar('D_acc_B', Dloss_B[1], step=step)
+                    if mse != -1:
+                        tf.summary.scalar('MSE_A', mse_A, step=step)
+                        tf.summary.scalar('MSE_B', mse_B, step=step)
+                        tf.summary.scalar('Identity_Loss', identity_loss, step=step)
+                        tf.summary.scalar('Pair_Matched_Loss', pair_matched_loss, step=step)
+                        tf.summary.scalar('MSE', mse, step=step)
+
+                # Save the checkpoint at given interval
+                if (step + 1) % int(interval*BATCH_SIZE) == 0:
+                    checkpoint.save(file_prefix=str(checkpoint_path))
+
+                # Schedule the learning rate
+                if (epoch + 1) % 100000 == 0:
+                    self.lr = self.lr_scheduler(self.lr, Type="Periodic", epoch=epoch, period=100000)
+                    # Kears callback: https://keras.io/zh/callbacks/
+
+                # Store the generated/reconstructed samples
+                if (step + 1) % 100 == 0:
+                    z_gen = np.random.normal(size=(int(NUM_IMG), self.latent_dim))
+                    A_gen_list.append(self.prediction(self.generatorA, z_gen))
+                    B_gen_list.append(self.prediction(self.generatorB, z_gen))
+                if mse != -1 and (step + 1) % 100 == 0:
+                    # Prediction
+                    A_rec = self.prediction(self.successorA, B_real)
+                    B_rec = self.prediction(self.successorB, A_real)
+                    for i in range(len(idx)):
+                        total_cnt += 1
+                        # Reshape image to 2D size
+                        A_rec_i = A_rec[i].reshape(self.img_shape[0], self.img_shape[1])
+                        B_rec_i = B_rec[i].reshape(self.img_shape[0], self.img_shape[1])
+                        # Get the binary masks of images
+                        A_rec_i_bi = convert2binary(A_rec_i, A_rec_i.max()*0.6)
+                        B_rec_i_bi = convert2binary(B_rec_i, B_rec_i.max()*0.3)
+                        # Compute the DSC
+                        DSC = dice_coefficient(A_rec_i_bi, B_rec_i_bi)
+                        # Compute matching_index & select rec samples
+                        if DSC < 0.2:
+                            match_cnt += 1
+                            AB_rec_list.append([A_rec[i], B_rec[i]])
+
+                # Plot the progress
+                print("A: No.{0}: D_loss: {1}; D_acc: {2}; G_loss: {3}."\
+                      .format(step, Dloss_A[0], Dloss_A[1], Gloss_A[0]))
+                print("B: No.{0}: D_loss: {1}; D_acc: {2}; G_loss: {3}."\
+                      .format(step, Dloss_B[0], Dloss_B[1], Gloss_B[0]))
                 if mse != -1:
-                    tf.summary.scalar('MSE_A', mse_A, step=epoch)
-                    tf.summary.scalar('MSE_B', mse_B, step=epoch)
-                    tf.summary.scalar('Identity_Loss', identity_loss, step=epoch)
-                    tf.summary.scalar('Pair_Matched_Loss', pair_matched_loss, step=epoch)
-                    tf.summary.scalar('MSE', mse, step=epoch)
+                    print("Total MSE: {0}.".format(mse))
+                print("----------")
 
-            # Save the checkpoint at given interval
-            if (epoch + 1) % int(interval) == 0:
-                checkpoint.save(file_prefix=str(checkpoint_path))
-
-            # Schedule the learning rate
-            if (epoch + 1) % 100000 == 0:
-                self.lr = self.lr_scheduler(self.lr, Type="Periodic", epoch=epoch, period=100000)
-                # Kears callback: https://keras.io/zh/callbacks/
-
-            # Store the generated/reconstructed samples
-            if (epoch + 1) % 50 == 0:
-                # Input random noise and generate A_gen/B_gen.
-                z_gen = np.random.normal(size=(int(NUM_IMG), self.latent_dim))
-                A_gen_list.append(self.prediction(self.generatorA, z_gen))
-                B_gen_list.append(self.prediction(self.generatorB, z_gen))
-            if mse != -1 and (epoch + 1) % 10 == 0:
-                # Prediction
-                A_rec = self.prediction(self.successorA, B_real)
-                B_rec = self.prediction(self.successorB, A_real)
-                for i in range(len(idx)):
-                    total_cnt += 1
-                    # Reshape image to 2D size
-                    A_rec_i = A_rec[i].reshape(self.img_shape[0], self.img_shape[1])
-                    B_rec_i = B_rec[i].reshape(self.img_shape[0], self.img_shape[1])
-                    # Get the binary masks of images
-                    A_rec_i_bi = convert2binary(A_rec_i, A_rec_i.max()*0.6)
-                    B_rec_i_bi = convert2binary(B_rec_i, B_rec_i.max()*0.3)
-                    # Compute the DSC
-                    DSC = dice_coefficient(A_rec_i_bi, B_rec_i_bi)
-                    # Compute matching_index & select rec samples
-                    if DSC < 0.2:
-                        match_cnt += 1
-                        AB_rec_list.append([A_rec[i], B_rec[i]])
-
-            # Plot the progress
-            print("A: No.{0}: D_loss: {1}; D_acc: {2}; G_loss: {3}."\
-                  .format(epoch, Dloss_A[0], Dloss_A[1], Gloss_A[0]))
-            print("B: No.{0}: D_loss: {1}; D_acc: {2}; G_loss: {3}."\
-                  .format(epoch, Dloss_B[0], Dloss_B[1], Gloss_B[0]))
-            if mse != -1:
-                print("Total MSE: {0}.".format(mse))
-            print("----------")
+        # Save file
+        np.save("./A_gen_baait.npy", A_gen_list)
+        np.save("./B_gen_baait.npy", B_gen_list)
+        np.save("./AB_rec_baait.npy", AB_rec_list)
+        checkpoint.save(file_prefix = str(checkpoint_path))
 
         # Evaluation
         print("Evaluation:")
-        matching_index = match_cnt/total_cnt
-        print("Matching Index: ", matching_index)
+        if total_cnt != 0:
+            matching_index = match_cnt/total_cnt
+            print("Matching Index: ", matching_index)
 
-        # Save file
-        np.save("./A_gen_banis.npy", A_gen_list)
-        np.save("./B_gen_banis.npy", B_gen_list)
-        np.save("./AB_rec_banis.npy", AB_rec_list)
-        checkpoint.save(file_prefix = str(checkpoint_path))
 
     def prediction(self, model, inputs):
         # Prediction with given model and inputs
